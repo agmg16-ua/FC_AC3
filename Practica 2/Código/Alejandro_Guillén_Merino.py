a54722a8428913a8ba94b78507b018df85258bc4
agmg16-ua <agmg16@alu.ua.es>
2023-11-27 12:33:06 +0100

    DecisionStump finished

--- Practica 2/Código/Alejandro_Guillén_Merino.py
+++ Practica 2/Código/Alejandro_Guillén_Merino.py
@@ -67,27 +67,33 @@ def load_MNIST_for_adaboost():
     Y_test = Y_test.astype("int8")
     return X_train, Y_train, X_test, Y_test
 
+
+##DECISION STUMP
 class DecisionStump:
     ## Constructor de clase, con número de características
     def __init__(self, n_features):
         # Seleccionar al azar una característica, un umbral y una polaridad.
-        self.feature_index = np.random.randint(0, n_features)
-        self.threshold = np.random.rand()
-        self.polarity = np.random.choice([-1, 1])
-        return self
+        self.caracteristica = np.random.randint(0, n_features)
+        self.umbral = np.random.rand()
+        self.polaridad = np.random.choice([-1, 1])
+        return None 
 
     ## Método para obtener una predicción con el clasificador débil
     def predict(self, X):
         # Si la característica que comprueba este clasificador es mayor que el umbral y la polaridad es 1
         # o si es menor que el umbral y la polaridad es -1, devolver 1 (pertenece a la clase)
         # Si no, devolver -1 (no pertenece a la clase)
-        predictions = np.ones(X.shape[0])
-        if self.polarity == 1:
-            predictions[X[:, self.feature_index] < self.threshold] = -1
-        else:
-            predictions[X[:, self.feature_index] >= self.threshold] = -1
-        return predictions
+        predicciones = []
+        for x in (X):
+            if (x[self.caracteristica] > self.umbral and self.polaridad == 1) or (x[self.caracteristica] < self.umbral and self.polaridad == -1):
+                predicciones.append(1)
+            else:
+                predicciones.append(-1)
 
+        return predicciones
+        
+
+##ADABOOST
 class Adaboost:
     ## Constructor de clase, con número de clasificadores e intentos por clasificador
     def __init__(self, T=5, A=20):
@@ -95,50 +101,15 @@ class Adaboost:
         self.T = T
         self.A = A
         self.classifiers = []
-        return self
+        return None
     
     ## Método para entrenar un clasificador fuerte a partir de clasificadores débiles mediante Adaboost
     def fit(self, X, Y, verbose = False):
         # Obtener el número de observaciones y de características por observación de X
-        n_observations, n_features = X.shape
-
+        n_observaciones, n_caracteristicas = X.shape
         # Iniciar pesos de las observaciones a 1/n_observaciones
-        weights = np.ones(n_observations) / n_observations
-
+        pesos = np.ones(n_observaciones) / n_observaciones
         # Bucle de entrenamiento Adaboost: desde 1 hasta T repetir
-        for t in range(self.T):
-            """
-            if verbose:
-                print("Entrenando clasificador débil " + str(t+1) + " de " + str(self.T))
-            # Iniciar lista de clasificadores débiles
-            classifiers = []
-            # Iniciar lista de predicciones ponderadas de los clasificadores débiles
-            predictions = np.zeros(n_observations)
-
-            # Bucle de búsqueda de un buen clasificador débil: desde 1 hasta A repetir
-            for a in range(self.A):
-                # Crear un nuevo clasificador débil aleatorio
-                classifier = DecisionStump(n_features)
-                # Calcular predicciones de ese clasificador para todas las observaciones
-                classifier_predictions = classifier.predict(X)
-                # Calcular el error: comparar predicciones con los valores deseados
-                error = np.sum(weights[classifier_predictions != Y])
-                # Actualizar mejor clasificador hasta el momento: el que tenga menor error
-                if error < best_error:
-                    best_classifier = classifier
-                    best_error = error
-                    best_predictions = classifier_predictions
-                # Calcular el valor de alfa y las predicciones del mejor clasificador débil
-                alpha = 0.5 * np.log((1.0 - best_error) / (best_error + 1e-10))
-                predictions += alpha * best_predictions
-                # Actualizar pesos de las observaciones en función de las predicciones, los valores deseados y alfa
-                weights *= np.exp(-alpha * Y * best_predictions)
-                # Normalizar a 1 los pesos
-                weights /= np.sum(weights)
-                # Guardar el clasificador en la lista de clasificadores de Adaboost
-                classifiers.append(best_classifier)
-            self.classifiers.append(classifiers)
-            """
         # Bucle de búsqueda de un buen clasificador débil: desde 1 hasta A repetir
         # Crear un nuevo clasificador débil aleatorio
         # Calcular predicciones de ese clasificador para todas las observaciones
@@ -149,10 +120,55 @@ class Adaboost:
         # Actualizar pesos de las observaciones en función de las predicciones, los valores deseados y alfa
         # Normalizar a 1 los pesos
         # Guardar el clasificador en la lista de clasificadores de Adaboost
+        pass
     
     ## Método para obtener una predicción con el clasificador fuerte Adaboost
     def predict(self, X):
         # Calcular las predicciones de cada clasificador débil para cada input multiplicadas por su alfa
-        predictions = np.array([classifier.predict(X) * classifier.alpha for classifier in self.classifiers])
         # Sumar para cada input todas las predicciones ponderadas y decidir la clase en función del signo
-        return np.sign(np.sum(predictions, axis=0))
+        pass
+
+
+def train_and_evaluate(clase, T, A, verbose=False):
+    # Cargar los datos de entrenamiento y test tal y como nos los sirve keras (MNIST de Yann Lecun)
+    X_train, Y_train, X_test, Y_test = load_MNIST_for_adaboost()
+
+    """
+    print(X_train.shape, X_train.dtype)
+    print(Y_train.shape, Y_train.dtype)
+    print(X_test.shape, X_test.dtype)
+    print(Y_test.shape, Y_test.dtype)
+    """
+
+    decisionStump = DecisionStump(784)
+
+    print(decisionStump.caracteristica)
+    print(decisionStump.umbral)
+    print(decisionStump.polaridad)
+
+    predicciones = decisionStump.predict(X_train)
+    print(predicciones)
+    # Crear un clasificador Adaboost con los parámetros T y A
+    """
+    adaboost = Adaboost(T, A)
+
+    # Entrenar el clasificador Adaboost con los datos de entrenamiento
+    adaboost.fit(X_train_clase, y_train_clase, verbose)
+
+    # Obtener las predicciones del clasificador Adaboost para los datos de entrenamiento y test
+    y_train_pred = adaboost.predict(X_train_clase)
+    y_test_pred = adaboost.predict(X_test_clase)
+
+    # Calcular las tasas de acierto para los datos de entrenamiento y test
+    train_accuracy = np.mean(y_train_pred == y_train_clase)
+    test_accuracy = np.mean(y_test_pred == y_test_clase)
+
+    # Imprimir las tasas de acierto
+    print("Tasa de acierto en datos de entrenamiento: {:.2f}%".format(train_accuracy * 100))
+    print("Tasa de acierto en datos de test: {:.2f}%".format(test_accuracy * 100))
+    """
+
+#Main
+if __name__ == "__main__":
+    # Entrenar y evaluar el clasificador Adaboost para la clase 1 con T = 5 y A = 20
+    train_and_evaluate(1, 5, 20, verbose=True)
