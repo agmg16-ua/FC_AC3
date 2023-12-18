5cd9c863ccb73e9008885adcf9149357d2a16b9a
agmg16-ua <agmg16@alu.ua.es>
2023-12-18 20:43:12 +0100

    Finalizado multiclase

--- Practica 2/Código/Alejandro_Guillén_Merino.py
+++ Practica 2/Código/Alejandro_Guillén_Merino.py
@@ -54,7 +54,7 @@ plot_X(features_fila_col, title, fila, columna)
 
 ##############################--Aqui empieza el código de la práctica--##############################
 
-def load_MNIST_for_adaboost():
+def load_MNIST_for_adaboost(clase, tipo):
     # Cargar los datos de entrenamiento y test tal y como nos los sirve keras (MNIST de Yann Lecun)
     (X_train, Y_train), (X_test, Y_test) = keras.datasets.mnist.load_data()
 
@@ -62,12 +62,19 @@ def load_MNIST_for_adaboost():
     X_train = X_train.reshape((X_train.shape[0], 28*28)).astype("float32") / 255.0
     X_test = X_test.reshape((X_test.shape[0], 28*28)).astype("float32") / 255.0
 
-    #X_train = X_train.astype("float32") / 255.0
-    #X_test = X_test.astype("float32") / 255.0
-    # Formatear las clases a enteros con signo para aceptar clase -1
-    Y_train = Y_train.astype("int8")
-    Y_test = Y_test.astype("int8")
-    return X_train, Y_train, X_test, Y_test
+    if tipo == "Binario":
+        # Filtrar las imágenes de la clase deseada
+        Y_train = (Y_train == clase).astype(int)*2 - 1
+        Y_test = (Y_test == clase).astype(int)*2 - 1 
+        return X_train, Y_train, X_test, Y_test
+    
+    if tipo == "Multiclase":
+        Y_train_vec = []
+        Y_test_vec = []
+        for i in range(clase):
+            Y_train_vec.append((Y_train == i).astype(int)*2 - 1)
+            Y_test_vec.append((Y_test == i).astype(int)*2 - 1)
+        return X_train, Y_train_vec, X_test, Y_test_vec, Y_train, Y_test
 
 
 ##DECISION STUMP
@@ -85,22 +92,8 @@ class DecisionStump:
         # Si la característica que comprueba este clasificador es mayor que el umbral y la polaridad es 1
         # o si es menor que el umbral y la polaridad es -1, devolver 1 (pertenece a la clase)
         # Si no, devolver -1 (no pertenece a la clase)
-        """
-        predicciones = []
-        for x in (X):
-            
-            if (x[self.caracteristica] > self.umbral and self.polaridad == 1) or (x[self.caracteristica] < self.umbral and self.polaridad == -1):
-                predicciones.append(1)
-            else:
-                predicciones.append(-1)
-
-        return predicciones
-        
-        """
-        #print("self.caracteristica = " + str(self.caracteristica))
-        caracteristicas_seleccionadas = X[:, self.caracteristica]
-        predicciones = np.where(self.polaridad * caracteristicas_seleccionadas > self.polaridad * self.umbral, 1, -1)
-        
+        caracteristicas = X[:, self.caracteristica]
+        predicciones = np.where(self.polaridad * caracteristicas > self.polaridad * self.umbral, 1, -1)
         return predicciones
                 
         
@@ -126,8 +119,9 @@ class Adaboost:
         # Bucle de entrenamiento Adaboost: desde 1 hasta T repetir
         for t in range(self.T):
             mejorError = 1
-            mejorClasificador = []
-            mejorPrediccion = []
+            mejorClasificador = ""
+            mejorPrediccion = ""
+
             clasificadoresT = []
             erroresT = []
             alphasT = []
@@ -152,14 +146,15 @@ class Adaboost:
                     mejorError = error
                 
             # Calcular el valor de alfa y las predicciones del mejor clasificador débil
-            alpha = 0.5 * np.log2((1 - mejorError) / mejorError)
+            alpha = 0.5 * np.log2((1 - mejorError) / max(mejorError, 1e-10))
             alphasT.append(alpha)
 
             # Actualizar pesos de las observaciones en función de las predicciones, los valores deseados y alfa
+            pesosAnt = np.sum(pesos)
             pesos = pesos * np.exp(-alpha * Y * mejorClasificador.predict(X))
 
             # Normalizar a 1 los pesos
-            pesos = pesos / np.sum(pesos)
+            pesos = pesos / pesosAnt
 
             # Guardar el clasificador y el alpha en la lista de clasificadores de Adaboost
             self.classifiers.append(mejorClasificador)
@@ -169,82 +164,63 @@ class Adaboost:
             if verbose: 
                 mensaje = "Añadido clasificador {:>3}: {:>4}, {:>6.4f}, {:+2}, {:>8.6f}".format(t+1, mejorClasificador.caracteristica, mejorClasificador.umbral, mejorClasificador.polaridad, mejorError)
                 print(mensaje)
+            
+            if verbose:
+                mensaje = "Iteración {:>3}/{:>3}, Error: {:>8.6f}".format(t+1, self.T, mejorError)
     
     ## Método para obtener una predicción con el clasificador fuerte Adaboost
-    def predict(self, X):
+    def predict(self, X, tipoAdaboost):
         # Calcular las predicciones de cada clasificador débil para cada input multiplicadas por su alfa
         # Sumar para cada input todas las predicciones ponderadas y decidir la clase en función del signo
-        """
-        predicciones = []
-        for x in (X):
-            predicciones.append(np.sign(np.sum([alpha * clasificador.predict(x) for alpha, clasificador in zip(self.alphas, self.classifiers)])))
-        return predicciones
-        """
-        """
-        aux = np.array([predicciones.predict(X) for predicciones, alfas in zip(self.classifiers, self.alphas)])
-        predicciones = np.sign(np.sum(aux, axis=0))
-        return predicciones
-        """
-        aux = np.array([alpha * clasificador.predict(X) for alpha, clasificador in zip(self.alphas, self.classifiers)])
-        predicciones = np.sign(np.sum(aux, axis=0))
+        if tipoAdaboost == "Binario":
+            predicciones = np.array([alpha * clasificador.predict(X) for alpha, clasificador in zip(self.alphas, self.classifiers)])
+            predicciones = np.sign(np.sum(predicciones, axis=0))
+
+        if tipoAdaboost == "Multiclase":
+            predicciones = np.sum([alpha * clasificador.predict(X) for alpha, clasificador in zip(self.alphas, self.classifiers)], axis=0)
+            
+
         return predicciones
 
+def precision(Y, Y_pred):
+    # Calcular la precisión como el número de aciertos sobre el total de predicciones
+    precision = np.sum(Y == Y_pred) / len(Y)
+    return precision
 
-def tareas_1A_y_1B_adaboost_binario(clase, T, A, verbose=False, graph=False):
-    # Cargar los datos de entrenamiento y test tal y como nos los sirve keras (MNIST de Yann Lecun)
-    X_train, Y_train, X_test, Y_test = load_MNIST_for_adaboost()
+def tareas_1A_y_1B_adaboost_binario(clase, T, A, verbose=False):
+    # Cargar los datos de entrenamiento y test tal y como nos los sirve keras (MNIST de Yann Lecun) para la clase deseada
+    X_train, Y_train, X_test, Y_test = load_MNIST_for_adaboost(clase, "Binario")
 
-    """
-    print(X_train.shape, X_train.dtype)
-    print(Y_train.shape, Y_train.dtype)
-    print(X_test.shape, X_test.dtype)
-    print(Y_test.shape, Y_test.dtype)
-    """
-    #Filtra los datos de entrenamiento y test para quedarnos solo con la clase que queremos
-    Y_train_clase = (Y_train == clase).astype(int)*2 - 1
-    Y_test_clase = (Y_test == clase).astype(int)*2 - 1  
 
     #Crea un clasificador Adaboost con los parámetros T y A y lo entrena con los datos de entrenamiento
     adaboost = Adaboost(T, A)
     if verbose:
         print("Entrenando clasificador Adaboost para el digito = " + str(clase) + " con T = " + str(T) + " y A = " + str(A) + "...")
     start = time.time()
-    adaboost.fit(X_train, Y_train_clase, verbose)
+    adaboost.fit(X_train, Y_train, verbose)
     end = time.time()
     total_time = end - start
 
     if verbose:
         print("Tiempo de entrenamiento: {:.2f} segundos".format(total_time))
-
-    """
-    decisionStump = DecisionStump(784)
-
-    print(decisionStump.caracteristica)
-    print(decisionStump.umbral)
-    print(decisionStump.polaridad)
-
-    predicciones = decisionStump.predict(X_train)
-    #print(predicciones)
-    """
-
+    
     # Obtener las predicciones del clasificador Adaboost para los datos de entrenamiento y test
-    y_train_pred = adaboost.predict(X_train)
-    y_test_pred = adaboost.predict(X_test)
+    y_train_pred = adaboost.predict(X_train, "Binario")
+    y_test_pred = adaboost.predict(X_test, "Binario")
 
-    # Calcular las tasas de acierto para los datos de entrenamiento y test
-    y_train_accuracy = np.mean(y_train_pred == Y_train_clase)
-    y_test_accuracy = np.mean(y_test_pred == Y_test_clase)
+    # Calcular la precisión para los datos de entrenamiento y test
+    y_train_accuracy = precision(Y_train, y_train_pred)
+    y_test_accuracy = precision(Y_test, y_test_pred)
 
     # Imprimir las tasas de acierto
     if verbose:
         print("Tasa de acierto (train, test) y tiempo: {:.2f}%, {:.2f}%, {:.3f} s.".format(y_train_accuracy * 100, y_test_accuracy * 100, total_time))
 
-    if graph:
-        return y_test_accuracy, total_time
+    return y_test_accuracy, total_time
 
 def tarea_1C_graficas_rendimiento(rend_1A):
-    valores_T = [5, 10, 15, 20, 25, 30, 40, 50, 75, 100]
-    valores_A = [5, 10, 15, 20, 25, 30, 40, 50, 75, 100]
+    valores_T = [1, 2, 5, 10, 15, 20, 25, 30, 45]
+    valores_A = [1, 2, 5, 10, 15, 20, 25, 30, 45]
     
     tasa_acierto_train = []
     tiempos_train = []
@@ -254,7 +230,7 @@ def tarea_1C_graficas_rendimiento(rend_1A):
         tiempos_aux = []
 
         for A in valores_A:
-            rendimiento, tiempo = tareas_1A_y_1B_adaboost_binario(clase=9, T=T, A=A, verbose=False, graph=True)
+            rendimiento, tiempo = tareas_1A_y_1B_adaboost_binario(clase=9, T=T, A=A, verbose=False)
             tasa_acierto_aux.append(rendimiento)
             tiempos_aux.append(tiempo)
         
@@ -285,44 +261,44 @@ def tarea_1C_graficas_rendimiento(rend_1A):
     plt.savefig('Grafica_Entrenamiento.png')
     print("\nSe ha guardado la gráfica de entrenamiento en el archivo Grafica_Entrenamiento.png")
     print("Se han establecido los valores de T=60 y A=15 como los óptimos\n")
-    t_optimo = 60
-    a_optimo = 15
+    t_optimo = 450
+    a_optimo = 2
 
     return t_optimo, a_optimo
 
-def tarea_1D_adaboost_multiclase(T, A):
-    # Cargar los datos de entrenamiento y test tal y como nos los sirve keras (MNIST de Yann Lecun)
-    X_train, Y_train, X_test, Y_test = load_MNIST_for_adaboost()
-
+def tarea_1D_adaboost_multiclase(T, A, verbose=False):
     #Se definen todas las clases
     clases = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    entrenoAdaboost = []
 
+    # Cargar los datos de entrenamiento y test tal y como nos los sirve keras (MNIST de Yann Lecun) para todas las clases
+    X_train, Y_train_vec, X_test, Y_test_vec, Y_train, Y_test = load_MNIST_for_adaboost(10, "Multiclase")
+
+    start = time.time()
     for clase in clases:
-        #Filtra los datos de entrenamiento y test para quedarnos solo con la clase que queremos
-        Y_train_clase = (Y_train == clase).astype(int)*2 - 1
-        Y_test_clase = (Y_test == clase).astype(int)*2 - 1  
-
+        #Entrenar adaboost para ese dígito
         adaboost = Adaboost(T, A)
-        #if verbose:
-        #    print("Entrenando clasificador Adaboost para el digito = " + str(clase) + " con T = " + str(T) + " y A = " + str(A) + "...")
-
-        start = time.time()
-        adaboost.fit(X_train, Y_train_clase, True)
-        end = time.time()
-        total_time = end - start
-
-        # Obtener las predicciones del clasificador Adaboost para los datos de entrenamiento y test
-        y_train_pred = adaboost.predict(X_train)
-        y_test_pred = adaboost.predict(X_test)
-
-        # Calcular las tasas de acierto para los datos de entrenamiento y test
-        y_train_accuracy = np.mean(y_train_pred == Y_train_clase)
-        y_test_accuracy = np.mean(y_test_pred == Y_test_clase)
-
-        # Imprimir las tasas de acierto
-        #if verbose:
-        #print("Tasa de acierto (train, test) y tiempo: {:.2f}%, {:.2f}%, {:.3f} s.".format(y_train_accuracy * 100, y_test_accuracy * 100, total_time))
+        if verbose:
+            print("Entrenando clasificador Adaboost para el digito = " + str(clase) + " con T = " + str(T) + " y A = " + str(A))
         
+        adaboost.fit(X_train, Y_train_vec[clase], True)
+        entrenoAdaboost.append(adaboost)
+    
+    end = time.time()
+    total_time = end - start
+
+    # Obtener las predicciones del clasificador Adaboost para los datos de entrenamiento y test
+    y_train_pred = np.argmax(np.array([adaboost.predict(X_train, "Multiclase") for adaboost in entrenoAdaboost]), axis=0)
+    y_test_pred = np.argmax(np.array([adaboost.predict(X_test, "Multiclase") for adaboost in entrenoAdaboost]), axis=0)
+
+    # Calcular las tasas de acierto para los datos de entrenamiento y test
+    y_train_accuracy = precision(Y_train, y_train_pred)
+    y_test_accuracy = precision(Y_test, y_test_pred)
+
+    # Imprimir las tasas de acierto
+    if verbose:
+        print("Tasa de acierto (train, test) y tiempo: {:.2f}%, {:.2f}%, {:.3f} s.".format(y_train_accuracy * 100, y_test_accuracy * 100, total_time))
+    
 
 
 #Main
@@ -341,8 +317,7 @@ if __name__ == "__main__":
     ## Se puede implementar reusando el código de las tareas 1A y 1B
     #tareas_1A_y_1B_adaboost_binario(clase=9, T=incognita, A=incognita)
     
-    
-    rend_1D = tarea_1D_adaboost_multiclase(T=valorT, A=valorA)
+    rend_1D = tarea_1D_adaboost_multiclase(T=valorT, A=valorA, verbose=True)
     """
     #rend_1E = tarea_1E_adaboost_multiclase_mejorado(T=incognita, A=incognita)
     #tarea_1E_graficas_rendimiento(rend_1D, rend_1E)
